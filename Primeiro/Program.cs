6cdc32dd2f39b09235a1b9b92fd6fe98ef5c1053
japuamelo <dimeloarquitetura@gmail.com>
2021-12-14 22:32:25 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Primeiro
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Primeira ";
+            var b = "aula em ";
+            var c = "C#";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(a + b + c);
+        }
+    }
+
+
+}
